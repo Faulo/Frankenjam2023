9b883b0f597208196875302a842d05dcec6a884f
Daniel Schulz <faulolio@gmail.com>
2023-11-04 01:10:14 +0100

    feat: fix webgl input

--- Assets/Scripts/Runtime/NewPlayerState.cs
+++ Assets/Scripts/Runtime/NewPlayerState.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace GossipGang {
     sealed class NewPlayerState : MonoBehaviour {
         [SerializeField]
-        TMP_InputField nameField;
+        InputField nameField;
         [SerializeField]
-        TMP_InputField birthdayField;
+        InputField birthdayField;
         [SerializeField]
-        TMP_InputField secretField;
+        InputField secretField;
         [SerializeField]
         Button nextPlayerButton;
         [SerializeField]
@@ -19,16 +18,44 @@ namespace GossipGang {
         bool isDone;
         bool nextPlayer;
 
+        [Space]
+        [SerializeField]
+        Color validColor = Color.white;
+        [SerializeField]
+        Color invalidColor = Color.red;
+
         void Start() {
             nextPlayerButton.onClick.AddListener(() => {
                 nextPlayer = true;
-                isDone = true;
+                isDone = Validate();
             });
 
             donePlayerButton.onClick.AddListener(() => {
-                isDone = true;
+                nextPlayer = false;
+                isDone = Validate();
             });
         }
+
+        bool Validate() {
+            bool isNameValid = ValidateString(nameField.text);
+            nameField.GetComponent<Image>().color = isNameValid
+                ? validColor
+                : invalidColor;
+
+            bool isBirthdayValid = ValidateString(birthdayField.text);
+            birthdayField.GetComponent<Image>().color = isBirthdayValid
+                ? validColor
+                : invalidColor;
+
+            bool isSecretValid = ValidateString(secretField.text);
+            secretField.GetComponent<Image>().color = isSecretValid
+                ? validColor
+                : invalidColor;
+
+            return isNameValid && isBirthdayValid && isSecretValid;
+        }
+
+        bool ValidateString(string text) => !string.IsNullOrWhiteSpace(text);
 
         public IEnumerator WaitForDone() {
             yield return new WaitUntil(() => isDone);
