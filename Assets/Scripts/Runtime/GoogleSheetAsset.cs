82f99a126bd00c1cdb8efa420518c276822a6ad8
Daniel Schulz <faulolio@gmail.com>
2023-11-05 11:51:49 +0100

    fix: fix logs

--- Assets/Scripts/Runtime/GoogleSheetAsset.cs
+++ Assets/Scripts/Runtime/GoogleSheetAsset.cs
@@ -161,7 +161,6 @@ namespace GossipGang {
                 UnityEditor.EditorUtility.SetDirty(day);
             }
 
-            UnityEditor.EditorUtility.SetDirty(this);
             UnityEditor.AssetDatabase.SaveAssets();
         }
 #endif
