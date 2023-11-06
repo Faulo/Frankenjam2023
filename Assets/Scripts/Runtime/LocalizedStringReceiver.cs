2793f9ecbf9f8632d42029d591658728f2c539a2
Daniel Schulz <faulolio@gmail.com>
2023-11-06 15:38:14 +0100

    fix: don't use WaitForCompletion in WebGL

--- Assets/Scripts/Runtime/LocalizedStringReceiver.cs
+++ Assets/Scripts/Runtime/LocalizedStringReceiver.cs
@@ -6,7 +6,7 @@ namespace GossipGang {
     sealed class LocalizedStringReceiver : MonoBehaviour, IBindingReceiver<LocalizedString> {
         public void Bind(LocalizedString model) {
             if (TryGetComponent<TMP_Text>(out var component)) {
-                component.text = model.GetLocalizedString();
+                component.text = "";
                 model.StringChanged += value => component.text = value;
             }
         }
